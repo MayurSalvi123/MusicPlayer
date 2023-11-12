0e88365294059dc9afeef791e7943a69c6a38552
MayurSalvi123 <msalvi660@gmail.com>
2023-11-12 20:09:31 +0530

    Imcomplete DbContext Setup

---
+++ API/MusicPlayerApi/MusicPlayerApi.DbContext/MusicPlayerDbContexts/MusicPlayerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlayerApi.Core.Entities;
+
+namespace MusicPlayerApi.DbContexts.MusicPlayerDbContexts
+{
+    public class MusicPlayerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("");
+        }
+    }
+}
