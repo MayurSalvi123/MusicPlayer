707bbef85fdfba6d430cfed5fe1a108d0a20375b
MayurSalvi123 <msalvi660@gmail.com>
2023-11-12 20:22:20 +0530

    Basic ConnectionStrings setup

--- API/MusicPlayerApi/MusicPlayerApi.DbContext/MusicPlayerDbContexts/MusicPlayerDbContext.cs
+++ API/MusicPlayerApi/MusicPlayerApi.DbContext/MusicPlayerDbContexts/MusicPlayerDbContext.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MusicPlayerApi.Core.Entities;
 
 namespace MusicPlayerApi.DbContexts.MusicPlayerDbContexts
 {
     public class MusicPlayerDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public MusicPlayerDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("");
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("MusicPlayerConnectionString"));
         }
     }
 }
