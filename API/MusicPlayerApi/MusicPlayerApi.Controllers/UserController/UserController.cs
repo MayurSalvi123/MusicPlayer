2d16365a2249583186c38c9476f42cc7ecd25b08
MayurSalvi123 <msalvi660@outlook.com>
2023-12-05 01:14:03 +0530

    Exposed UserController

--- API/MusicPlayerApi/MusicPlayerApi.Controllers/UserController/UserController.cs
+++ API/MusicPlayerApi/MusicPlayerApi.Controllers/UserController/UserController.cs
@@ -1,12 +1,14 @@
-﻿using System.Web.Mvc;
-using AutoMapper;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using MusicPlayer.Managers.Interfaces;
 using MusicPlayerApi.Core.Dtos.User;
 using MusicPlayerApi.Core.Entities;
 
 namespace MusicPlayerApi.Controllers.UserController
 {
-    public class UserController : Controller
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
     {
         private readonly IUserManager _userManager;
         private readonly IMapper _mapper;
@@ -17,22 +19,25 @@ namespace MusicPlayerApi.Controllers.UserController
             _mapper = mapper;
         }
 
-
+        [HttpPost]
         public async Task<UserDto> CreateUser(CreateUserDto userDto)
         {
             var userObject = _mapper.Map<User>(userDto);
             var createdUser = await _userManager.AddUser(userObject).ConfigureAwait(false);
             return _mapper.Map<UserDto>(createdUser);
         }
-        public async Task<List<UserDto>>GetAllUsers(FetchUserDto fetchUserDto)
+        [HttpGet]
+        public async Task<List<UserDto>>GetAllUsers()
         {
             var users = await _userManager.GetAll().ConfigureAwait(false);
             return _mapper.Map<List<UserDto>>(users);
         }
+        [HttpDelete]
         public async Task RemoveUser(RemoveUserDto userDto)
         {
             await _userManager.DeleteUser(userDto.UserId).ConfigureAwait(false);
         }
+        [HttpPost]
         public async Task<UserDto> UpdateUser(UpdateUserDto userDto)
         {
             var userObject = _mapper.Map<User>(userDto);
--- API/MusicPlayerApi/MusicPlayerApi/Program.cs
+++ API/MusicPlayerApi/MusicPlayerApi/Program.cs
@@ -14,7 +14,16 @@ namespace MusicPlayerApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo 
+                    {
+                        Title = "Music Player APIs",
+                        Description = "Music Player Swagger Endpoints",
+                        Version = "v1"
+                    });
+            });
 
             builder.Services.AddDbContext<MusicPlayerDbContext>();
 
@@ -24,17 +33,26 @@ namespace MusicPlayerApi
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Music Player");
+                });
             }
 
             app.MapSwagger();
 
             app.UseHttpsRedirection();
 
+            //app.UseRouting();
+
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
 
             app.Run();
         }
